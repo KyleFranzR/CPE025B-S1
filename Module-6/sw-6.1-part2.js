3e30e377ef77826ca04cf027f6762db8e621778e
Freuncze <kylefranzrodriguez@gmail.com>
2026-02-27 15:53:46 +0800

    chore: added Seatwork 6.1

---
+++ Module-6/sw-6.1-part2.js
@@ -0,0 +1,5 @@
+let width = prompt("Enter width", 0);
+let height = prompt("Enter height", 0);
+let length = prompt("Enter length", 0);
+let volume = width * height * length;
+alert(`Calculated volume is ${volume}`);
